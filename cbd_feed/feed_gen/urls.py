3742d946cd1c369b366948c75b02dade75fbf46c
Charley Walton <chazeah@gmail.com>
2017-11-17 09:22:24 +0800

    initial feed, with escaped descriptions

--- cbd_feed/feed_gen/urls.py
+++ cbd_feed/feed_gen/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.feed, name='feed'),
+    url(r'^$', views.LatestEntriesFeed()),
 ]
--- cbd_feed/feed_gen/views.py
+++ cbd_feed/feed_gen/views.py
@@ -1,6 +1,25 @@
-# from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.syndication.views import Feed
+from content_parser.models import CBDPost
 
 
-def feed(request):
-    return HttpResponse('Hello, world. You are at the feed index.')
+class LatestEntriesFeed(Feed):
+    title = 'Crunchbase Daily'
+    description = 'Feed containing data from the daily Crunchbase email.'
+    link = '/feed/'
+
+    def items(self):
+        return CBDPost.objects.all()[:5]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.body_content
+
+    def item_pubdate(self, item):
+        return item.published_at
+
+    def item_link(self, item):
+        post_id = 'https://cbd-feed.herokuapp.com/entry/{}'.format(item.id)
+        return post_id
