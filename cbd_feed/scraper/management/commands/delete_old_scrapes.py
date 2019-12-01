4e400bda2021ed551744d866921d8676bd8aeb73
Charley Walton <chazeah@gmail.com>
2019-11-30 21:05:59 -0700

    reducing scrape keep to 30 days

--- cbd_feed/scraper/management/commands/delete_old_scrapes.py
+++ cbd_feed/scraper/management/commands/delete_old_scrapes.py
@@ -12,8 +12,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print('Deleting old scrapes...')
         today = datetime.now(pytz.utc)
-        year_ago = today - timedelta(days=365)
-        old_scrapes = Result.objects.filter(created_at__lt=year_ago)
+        month_ago = today - timedelta(days=30)
+        old_scrapes = Result.objects.filter(created_at__lt=month_ago)
         num_deleted = old_scrapes.delete()[0];
 
         print('Deleted {} old scrapes'.format(num_deleted))
